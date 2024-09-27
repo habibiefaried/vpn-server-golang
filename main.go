100828bf1a87da4ad911fca841144af97462b90d
Habibie Faried <habibiefaried@gmail.com>
2024-09-27 07:39:10 +0000

    feat: finale

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strings"
 	"time"
 
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 	"github.com/songgao/water"
 	"github.com/spf13/pflag"
 )
@@ -56,16 +59,50 @@ func main() {
 		log.Println("Connected to server")
 
 		// Handle bidirectional communication
+		IpParts := strings.Split(*netIp, "/")
+		sendTCPMessage(conn, IpParts[0])
 		go ReadIfaceAndSendTCP(ifce, conn)
 		RecvTCPAndWriteIface(conn, ifce)
 
 	} else {
+		connectionsPool := make(map[string]net.Conn)
+
 		// Accept client connections in a loop
 		listener, err := net.Listen("tcp", fmt.Sprintf("%v:%v", *host, *port))
 		if err != nil {
 			log.Fatalf("Failed to listen on port %v: %v", *port, err)
 		}
 		log.Printf("Listening on port %v\n", *port)
+
+		go func() {
+			rp := make([]byte, MTU)
+			for {
+				n, err := ifce.Read(rp) // TODO: THIS MUST MOVE OUT FROM HERE IF WE WANT MULTIPLE USER
+				if err != nil {
+					log.Printf("Error reading from TUN interface: %v", err)
+					n = 0
+				}
+
+				if n > 0 {
+					packet := gopacket.NewPacket(rp[:n], layers.LayerTypeIPv4, gopacket.Default)
+
+					// Check if the packet contains an IPv4 layer
+					if ipLayer := packet.Layer(layers.LayerTypeIPv4); ipLayer != nil {
+						ip, _ := ipLayer.(*layers.IPv4)
+
+						fmt.Printf("IPv4 packet: Src: %s, Dest: %s\n", ip.SrcIP, ip.DstIP)
+						// Send the data to the TCP server
+						// TODO: Add retry / timeout
+						_, err = connectionsPool[ip.DstIP.String()].Write(rp[:n])
+						if err != nil {
+							log.Printf("Error sending packet to TCP server: %v", err)
+						}
+					} else {
+						fmt.Println("Not an IPv4 packet")
+					}
+				}
+			}
+		}()
 
 		for {
 			conn, err := listener.Accept()
@@ -78,8 +115,12 @@ func main() {
 			// Handle each client connection in a new goroutine
 			go func() {
 				defer conn.Close()
-				go RecvTCPAndWriteIface(conn, ifce)
-				ReadIfaceAndSendTCP(ifce, conn)
+				netIp := recvTCPMessage(conn)
+				log.Println("Got IP from " + netIp)
+
+				connectionsPool[netIp] = conn
+
+				RecvTCPAndWriteIface(connectionsPool[netIp], ifce)
 			}()
 		}
 	}

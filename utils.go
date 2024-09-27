5dd9568a7767725194e7f75c709a5e7c537cfe8f
Habibie Faried <habibiefaried@gmail.com>
2024-09-27 10:44:20 +0700

    [F-2] change method of send and receive packet (#2)

--- utils.go
+++ utils.go
@@ -1,18 +1,54 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"log"
+	"net"
 	"os/exec"
+
+	"github.com/songgao/water"
 )
 
-// transfer data between two interfaces (TUN or TCP)
-func transfer(src io.Reader, dst io.Writer) {
-	n, err := io.Copy(dst, src)
-	if err != nil {
-		log.Printf("Transfer error: %v", err)
+func ReadIfaceAndSendTCP(ifce *water.Interface, conn net.Conn) {
+	packet := make([]byte, MTU)
+	for {
+		n, err := ifce.Read(packet)
+		if err != nil {
+			log.Printf("Error reading from TUN interface: %v", err)
+			n = 0
+		}
+
+		fmt.Printf("Read %d bytes from TUN interface\n", n)
+
+		if n > 0 {
+			// Send the data to the TCP server
+			_, err = conn.Write(packet[:n])
+			if err != nil {
+				log.Printf("Error sending packet to TCP server: %v", err)
+			}
+		}
 	}
-	log.Printf("Transferred %d bytes", n)
+}
+
+func RecvTCPAndWriteIface(conn net.Conn, ifce *water.Interface) {
+	response := make([]byte, 1500)
+	for {
+		n, err := conn.Read(response)
+		if err != nil {
+			log.Printf("Error reading from TCP server: %v", err)
+			n = 0
+		}
+
+		fmt.Printf("Received %d bytes from TCP server\n", n)
+
+		if n > 0 {
+			// Write the response back to the TUN interface
+			_, err = ifce.Write(response[:n])
+			if err != nil {
+				log.Printf("Error writing to TUN interface: %v", err)
+			}
+		}
+	}
 }
 
 func runIP(args ...string) {
